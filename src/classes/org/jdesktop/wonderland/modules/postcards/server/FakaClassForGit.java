3a759a1f048626031148a5e419085dafa1a6390b
OpenWonderland Wednesdays <wednesdays@openwonderland.org>
2012-10-11 13:54:39 -0500

    Test for Netbeans-2

---
+++ src/classes/org/jdesktop/wonderland/modules/postcards/server/FakaClassForGit.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.jdesktop.wonderland.modules.postcards.server;
+
+/**
+ *
+ * @author spcworld
+ */
+public class FakaClassForGit {
+
+}
