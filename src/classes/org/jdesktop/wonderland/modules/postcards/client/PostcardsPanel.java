9ea90cf61d93330e393877b4532f5805231e67c5
OpenWonderland Wednesdays <wednesdays@openwonderland.org>
2012-10-15 13:38:44 -0500

    changes in the hud, swapped awt components for swing components

--- src/classes/org/jdesktop/wonderland/modules/postcards/client/PostcardsPanel.java
+++ src/classes/org/jdesktop/wonderland/modules/postcards/client/PostcardsPanel.java
@@ -32,27 +32,24 @@ public class PostcardsPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButton1 = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
         shootPanel = new javax.swing.JPanel();
         viewPanel = new javax.swing.JPanel();
-        captureButton = new java.awt.Button();
-        postcardName = new java.awt.TextField();
-        postcardMessage = new java.awt.TextField();
-        addBannerCheckbox = new java.awt.Checkbox();
-        addURLCheckbox = new java.awt.Checkbox();
         saveButton = new javax.swing.JButton();
         annotateButton = new javax.swing.JButton();
-        exportButton = new javax.swing.JButton();
-        checkbox1 = new java.awt.Checkbox();
-
-        jButton1.setText("jButton1");
+        sendToButton = new javax.swing.JButton();
+        captureButton = new javax.swing.JButton();
+        postcardName = new javax.swing.JTextField();
+        postcardMessage = new javax.swing.JTextField();
+        addBannerCheckBox = new javax.swing.JCheckBox();
+        addOWLURL = new javax.swing.JCheckBox();
+        shareWithAll = new javax.swing.JCheckBox();
 
         javax.swing.GroupLayout shootPanelLayout = new javax.swing.GroupLayout(shootPanel);
         shootPanel.setLayout(shootPanelLayout);
         shootPanelLayout.setHorizontalGroup(
             shootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 327, Short.MAX_VALUE)
+            .addGap(0, 423, Short.MAX_VALUE)
         );
         shootPanelLayout.setVerticalGroup(
             shootPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -65,7 +62,7 @@ public class PostcardsPanel extends javax.swing.JPanel {
         viewPanel.setLayout(viewPanelLayout);
         viewPanelLayout.setHorizontalGroup(
             viewPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 327, Short.MAX_VALUE)
+            .addGap(0, 423, Short.MAX_VALUE)
         );
         viewPanelLayout.setVerticalGroup(
             viewPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -74,63 +71,56 @@ public class PostcardsPanel extends javax.swing.JPanel {
 
         jTabbedPane1.addTab("View", viewPanel);
 
-        captureButton.setActionCommand("captureImage");
-        captureButton.setLabel("Capture");
-
-        postcardName.setText("Name your postcard here");
-
-        postcardMessage.setText("Write your message here");
-
-        addBannerCheckbox.setLabel("Add Banner");
-
-        addURLCheckbox.setLabel("Add OWL URL");
-
         saveButton.setText("Save");
 
         annotateButton.setText("Annotate");
 
-        exportButton.setText("Send to ..");
-        exportButton.addActionListener(new java.awt.event.ActionListener() {
+        sendToButton.setText("Send to ..");
+        sendToButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                exportButtonActionPerformed(evt);
+                sendToButtonActionPerformed(evt);
             }
         });
 
-        checkbox1.setLabel("Share With All");
+        captureButton.setText("Capture");
+
+        postcardName.setText("Name your postcard here");
+
+        postcardMessage.setText("write your message here");
+
+        addBannerCheckBox.setText("Add Banner");
+
+        addOWLURL.setText("Add OWL URL");
+
+        shareWithAll.setText("Share with All");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(161, 161, 161)
-                .addComponent(captureButton, javax.swing.GroupLayout.PREFERRED_SIZE, 76, javax.swing.GroupLayout.PREFERRED_SIZE))
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                    .addGap(29, 29, 29)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(annotateButton)
-                                .addComponent(addBannerCheckbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addGap(38, 38, 38)
-                                    .addComponent(saveButton, javax.swing.GroupLayout.DEFAULT_SIZE, 99, Short.MAX_VALUE))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                    .addComponent(addURLCheckbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                .addComponent(exportButton)
-                                .addComponent(checkbox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                            .addComponent(postcardMessage, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(postcardName, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 356, Short.MAX_VALUE)))
-                    .addContainerGap(22, Short.MAX_VALUE))
-                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                    .addGap(39, 39, 39)
-                    .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 332, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(29, 29, 29)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jTabbedPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(addBannerCheckBox)
+                        .addGap(18, 18, 18)
+                        .addComponent(addOWLURL)
+                        .addGap(30, 30, 30)
+                        .addComponent(shareWithAll))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(annotateButton, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 34, Short.MAX_VALUE)
+                        .addComponent(saveButton, javax.swing.GroupLayout.PREFERRED_SIZE, 156, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(sendToButton))
+                    .addComponent(postcardMessage, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE)
+                    .addComponent(postcardName, javax.swing.GroupLayout.DEFAULT_SIZE, 428, Short.MAX_VALUE))
+                .addGap(44, 44, 44))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(183, 183, 183)
+                .addComponent(captureButton)
+                .addContainerGap(227, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -138,43 +128,41 @@ public class PostcardsPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(captureButton, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(captureButton)
+                .addGap(8, 8, 8)
+                .addComponent(postcardName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(postcardMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(15, 15, 15)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(addBannerCheckBox)
+                    .addComponent(addOWLURL)
+                    .addComponent(shareWithAll))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(postcardName, javax.swing.GroupLayout.PREFERRED_SIZE, 19, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(postcardMessage, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(addBannerCheckbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(addURLCheckbox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(checkbox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(annotateButton)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(saveButton)
-                        .addComponent(exportButton)))
-                .addContainerGap())
+                    .addComponent(sendToButton)
+                    .addComponent(saveButton))
+                .addContainerGap(21, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void exportButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exportButtonActionPerformed
+    private void sendToButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sendToButtonActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_exportButtonActionPerformed
+    }//GEN-LAST:event_sendToButtonActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private java.awt.Checkbox addBannerCheckbox;
-    private java.awt.Checkbox addURLCheckbox;
+    private javax.swing.JCheckBox addBannerCheckBox;
+    private javax.swing.JCheckBox addOWLURL;
     private javax.swing.JButton annotateButton;
-    private java.awt.Button captureButton;
-    private java.awt.Checkbox checkbox1;
-    private javax.swing.JButton exportButton;
-    private javax.swing.JButton jButton1;
+    private javax.swing.JButton captureButton;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private java.awt.TextField postcardMessage;
-    private java.awt.TextField postcardName;
+    private javax.swing.JTextField postcardMessage;
+    private javax.swing.JTextField postcardName;
     private javax.swing.JButton saveButton;
+    private javax.swing.JButton sendToButton;
+    private javax.swing.JCheckBox shareWithAll;
     private javax.swing.JPanel shootPanel;
     private javax.swing.JPanel viewPanel;
     // End of variables declaration//GEN-END:variables
